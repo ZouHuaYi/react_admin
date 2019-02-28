0beb39ce3dcb0dd37b1023a60f721ea3577f5165
zhy <1004753639@qq.com>
2019-02-28 18:16:30 +0800

    2/28

---
+++ src/defaultSettings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "navTheme": "dark",
+  "primaryColor": "#1890FF",
+  "layout": "sidemenu",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "autoHideHeader": false,
+  "fixSiderbar": true,
+  "menu": {
+  "disableLocal": false
+},
+  "title": "美上美",
+  "pwa": true,
+  "iconfontUrl": "",
+  "collapse": true
+};
